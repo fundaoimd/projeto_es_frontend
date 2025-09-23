56bb61c033cba712ad92d3c32ffa13c1a353e569
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-09-23 15:53:45 -0300

    feat: criando botão de navegação inicial #24

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
+
 <script setup lang="ts">
-// Importa o nosso novo componente.
+import { ref } from 'vue';
 import ClientCRUD from './components/ClientCRUD.vue';
+const showClientCRUD = ref(false);
+function openClientCRUD() {
+  showClientCRUD.value = true;
+}
 </script>
 
 <template>
   <main>
-    <ClientCRUD />
+    <h1 style="text-align:center; margin-bottom: 2rem;">Sistema de Cadastro - Gloria Maria</h1>
+    <div v-if="!showClientCRUD">
+      <button @click="openClientCRUD" style="display:block; margin: 0 auto 2rem auto; padding: 1rem 2rem; font-size: 1.2rem;">Ir para Cadastro de Cliente</button>
+    </div>
+    <div v-else style="display: flex; flex-direction: column; min-height: 70vh;">
+      <ClientCRUD />
+      <button 
+        @click="showClientCRUD = false" 
+        style="margin-top: auto; align-self: flex-end; padding: 0.7rem 1.5rem; font-size: 1rem;">
+        Voltar
+      </button>
+    </div>
   </main>
 </template>
 
