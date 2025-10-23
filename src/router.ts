5d6d268e80cd330c6c68433364c067de37552e36
hildemberg986 <hildembergeling@gmail.com>
2025-10-23 15:37:24 -0300

    update rotas

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    { 
-        path: '/', 
-        name: 'Home', 
-        component: () => import('./pages/MyHome.vue')
-    }
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('./pages/MyHome.vue'),
+  },
+  {
+    path: '/cliente',
+    name: 'Cliente',
+    component: () => import('./pages/MenuCliente.vue'),
+  },
+  {
+    path: '/agendamento',
+    name: 'Agendamento',
+    component: () => import('./pages/MenuAgendamento.vue'),
+  },
+  {
+    path: '/funcionario',
+    name: 'Funcionario',
+    component: () => import('./pages/MenuFuncionario.vue'),
+  },
 ]
 
 const router = createRouter({
